1c8cfb88489850fc25ea96a14f227b6515e2a81c
Xetra <xetrazx@gmail.com>
2025-02-22 00:21:17 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ chmod +x $HOME/start_x11.sh
 echo "
 export DISPLAY=:0
 virgl_test_server_android &
-sleep 3
 termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session" &
 " >> $HOME/start_x11.sh
 clear
