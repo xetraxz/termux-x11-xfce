75391669803fb4bf112be6411826b54dbfc7c39d
Xetra <xetrazx@gmail.com>
2025-02-22 00:30:05 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,7 @@ pkg install virglrenderer-android -y
 touch $HOME/start_x11.sh
 chmod +x $HOME/start_x11.sh
 #
-echo "
-virgl_test_server_android &
+echo "virgl_test_server_android &
 termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session" &
 " >> $HOME/start_x11.sh
 clear
