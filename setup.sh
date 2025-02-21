40a8457bbf94ae55be27fbbfa687ea7ed8f40b06
Xetra <xetrazx@gmail.com>
2025-02-22 00:32:48 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ touch $HOME/start_x11.sh
 chmod +x $HOME/start_x11.sh
 #
 echo "virgl_test_server_android &
-termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session" &
+termux-x11 :0 -xstartup 'dbus-launch --exit-with-session xfce4-session' &
 " >> $HOME/start_x11.sh
 clear
 echo "Setup Completed"
